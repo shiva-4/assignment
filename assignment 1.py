2774fa346cca455e7d18b14469895b2bc9a73f34
shiva krishna <shivakrishnanangunuri@gmail.com>
2021-01-09 21:18:19 +0530

    second commit

---
+++ assignment 1.py
@@ -0,0 +1,36 @@
+user='india'
+password='ind123'
+i=1
+
+while(1):
+    userinput=input("enter username")
+    userpassword=input("enter password")
+
+
+
+    
+    if userinput=='india' and userpassword=='ind123' :
+         print("congratulations login sucessful")
+         break
+    elif userinput=='india' and userpassword!='ind123' :
+        print("incorrect password")
+        if i==2:
+            print("ab")
+    else :
+        print("incorrect credentials")
+    
+        
+            
+       
+
+            
+
+             
+        
+
+   
+
+   
+       
+
+  
