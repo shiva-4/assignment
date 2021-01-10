3160e19bac0ff924b4ce7895c5217d5bfe70a1a3
shiva-4 <77113270+shiva-4@users.noreply.github.com>
2021-01-10 14:31:57 +0530

    Update assignment 1.py

--- assignment 1.py
+++ assignment 1.py
@@ -1,6 +1,6 @@
 user='india'
 password='ind123'
-i=1
+
 
 while(1):
     userinput=input("enter username")
@@ -14,8 +14,7 @@ while(1):
          break
     elif userinput=='india' and userpassword!='ind123' :
         print("incorrect password")
-        if i==2:
-            print("ab")
+       
     else :
         print("incorrect credentials")
     
